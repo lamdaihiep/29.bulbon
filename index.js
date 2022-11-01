df6ab77ae2e7127a4e007b09875b0f5efd4158de
lamdaihiep <115853661+lamdaihiep@users.noreply.github.com>
2022-11-01 16:06:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client';
+
+function click_abc(){
+
+  let a = document.getElementById('myimage');
+  if(!a)
+    return;
+
+  if(a.src !== require('./bulbon.gif'))
+    a.src = require("./bulbon.gif");
+  else
+    a.src = require("./bulboff.gif");
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <img id="myimage" onClick={click_abc} src={require("./bulboff.gif")} width="100" height="180" />
+  
+);
